c050dd887d411f0167908042ef3d99cae3001dcb
Lucas Merchant <lbmerchant93@gmail.com>
2023-03-13 12:36:46 -0600

    Write test for log in directions

--- src/pages/LandingPage/LandingPage.test.tsx
+++ src/pages/LandingPage/LandingPage.test.tsx
@@ -11,9 +11,13 @@ describe('Landing Page', () => {
 
     test('Renders Squirrel Saver description.', () => {
         render(<LandingPage />);
-        const pageTitle = screen.getByText("This app is designed to help you squirrel away money from each shift you work in order to help build your savings. ");
+        const pageTitle = screen.getByText("This app is designed to help you squirrel away money from each shift you work in order to help build your savings. You can create an account below to get started and follow the instructions for setting up your account. Already have an account? Great! Welcome back! Log in below to go to your profile.");
         expect(pageTitle).toBeInTheDocument();
     });
 
-
+    test('Renders log in directions.', () => {
+        render(<LandingPage />);
+        const pageTitle = screen.getByText("Already have an account? Great! Welcome back! Click here to log in.");
+        expect(pageTitle).toBeInTheDocument();
+    });
 })
