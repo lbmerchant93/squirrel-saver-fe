148642dc9211781236bb75cbf5f2b849af7c49dd
Lucas Merchant <lbmerchant93@gmail.com>
2023-03-13 12:35:22 -0600

    Write tests for LandingPage welcome message and description

--- src/pages/LandingPage/LandingPage.test.tsx
+++ src/pages/LandingPage/LandingPage.test.tsx
@@ -3,9 +3,17 @@ import { render, screen } from '@testing-library/react';
 import LandingPage from './LandingPage';
 
 describe('Landing Page', () => {
-    test('Renders Landing Page text', () => {
+    test('Renders welcome message.', () => {
         render(<LandingPage />);
-        const pageTitle = screen.getByText("Landing Page");
+        const pageTitle = screen.getByText("Welcome to Squirrel Saver!");
         expect(pageTitle).toBeInTheDocument();
     });
+
+    test('Renders Squirrel Saver description.', () => {
+        render(<LandingPage />);
+        const pageTitle = screen.getByText("This app is designed to help you squirrel away money from each shift you work in order to help build your savings. ");
+        expect(pageTitle).toBeInTheDocument();
+    });
+
+
 })
