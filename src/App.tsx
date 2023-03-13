f5f00044847db8230eafd5792089f46fb5caaec6
Lucas Merchant <lbmerchant93@gmail.com>
2023-03-13 13:08:08 -0600

    Create Routes for LoginPage and CreateAccountPage

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { PossibleRoutes } from './utils/constants';
 import AppBar from './features/AppBar/AppBar';
+import CreateAccountPage from './pages/CreateAccountPage/CreateAccountPage';
 import LandingPage from './pages/LandingPage/LandingPage';
+import LoginPage from './pages/LoginPage/LoginPage';
 import { initializeApp } from 'firebase/app';
 import firebaseConfig from './configs/firebase.configs';
 import AuthProvider from './App.authProvider';
@@ -15,6 +17,8 @@ const App = () => {
   const routes = (
     <Routes>
       <Route path={`${PossibleRoutes.ROOT}`} element={<LandingPage />} />
+      <Route path={`${PossibleRoutes.CREATE_ACCOUNT}`} element={<CreateAccountPage />} />
+      <Route path={`${PossibleRoutes.LOGIN}`} element={<LoginPage />} />
     </Routes>
   );
 
