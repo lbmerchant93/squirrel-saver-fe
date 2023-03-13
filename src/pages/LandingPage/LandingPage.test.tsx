05fa47f01e7cf5ec582558c23d7b55e8ba6746b2
Lucas Merchant <lbmerchant93@gmail.com>
2023-03-13 12:55:37 -0600

    Add test for links be present for LoginPage and CreateAccountPage

--- src/pages/LandingPage/LandingPage.test.tsx
+++ src/pages/LandingPage/LandingPage.test.tsx
@@ -5,25 +5,31 @@ import LandingPage from './LandingPage';
 describe('Landing Page', () => {
     test('Renders welcome message.', () => {
         render(<LandingPage />);
-        const pageTitle = screen.getByText("Welcome to Squirrel Saver!");
-        expect(pageTitle).toBeInTheDocument();
+        const welcomeMessage = screen.getByText("Welcome to Squirrel Saver!");
+        expect(welcomeMessage).toBeInTheDocument();
     });
 
     test('Renders Squirrel Saver description.', () => {
         render(<LandingPage />);
-        const pageTitle = screen.getByText("This app is designed to help you squirrel away money from each shift you work in order to help build your savings.");
-        expect(pageTitle).toBeInTheDocument();
+        const description = screen.getByText("This app is designed to help you squirrel away money from each shift you work in order to help build your savings.");
+        expect(description).toBeInTheDocument();
     });
 
-    test('Renders log in directions.', () => {
+    test('Renders log in directions and link to LoginPage.', () => {
         render(<LandingPage />);
-        const pageTitle = screen.getByText("Already have an account? Great! Welcome back! Click here to log in.");
-        expect(pageTitle).toBeInTheDocument();
+        const loginDirections = screen.getByText("Already have an account? Great! Welcome back! Click here to login.");
+        expect(loginDirections).toBeInTheDocument();
+
+        const loginLink = screen.getAllByRole('link', { name: 'login'})
+        expect(loginLink).toBeInTheDocument();
     });
 
-    test('Renders create account directions.', () => {
+    test('Renders create account directions and link to CreateAccountPage.', () => {
         render(<LandingPage />);
-        const pageTitle = screen.getByText("Is this your first time here? Awesome, welcome to Squirrel Saver! Click here to create an account and get started squirreling your way to a better savings.");
-        expect(pageTitle).toBeInTheDocument();
+        const createAccountDirections = screen.getByText("Is this your first time here? Awesome, welcome to Squirrel Saver! Click here to create an account and get started squirreling your way to a better savings.");
+        expect(createAccountDirections).toBeInTheDocument();
+
+        const createAccountLink = screen.getAllByRole('link', { name: 'create an account'})
+        expect(createAccountLink).toBeInTheDocument();
     });
 })
