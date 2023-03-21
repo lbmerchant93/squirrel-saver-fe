73de60686686a3156a765258f9774739db5038a6
Lucas Merchant <lbmerchant93@gmail.com>
2023-03-21 13:27:55 -0600

    Write test for welcome message and directions being visible

--- src/pages/LoginPage/LoginPage.test.tsx
+++ src/pages/LoginPage/LoginPage.test.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import LoginPage from './LoginPage';
 
-it('Renders title', () => {
-    render(<LoginPage />);
-    const linkElement = screen.getByText("LoginPage");
-    expect(linkElement).toBeInTheDocument();
+describe('Login Page', () => {
+    it('Renders a welcome back message and directs the user to login through their Google account or their email/password.', () => {
+        render(<LoginPage />);
+        const welcomeMessage = screen.getByText(/Welcome back!/i);
+        expect(welcomeMessage).toBeInTheDocument();
+        const loginDirections = screen.getByText("Please log in to access your account! You can log in through your Google account or enter your email/password");
+        expect(loginDirections).toBeInTheDocument();
+    });
 });
