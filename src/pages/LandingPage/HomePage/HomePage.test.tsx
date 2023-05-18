15ba436bdd75d5aa1c4446e8aeae8b58dae6a0b2
Lucas Merchant <lbmerchant93@gmail.com>
2023-05-18 11:01:08 -0600

    Create HomePage folder under LandingPage, condition for if user.isLoggedIn

---
+++ src/pages/LandingPage/HomePage/HomePage.test.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+describe('Landing Page', () => {
+    it.skip('Renders welcome back user message.', () => {
+
+    });
+})
--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Typography from '@mui/material/Typography';
 import { PossibleRoutes } from '../../utils/constants';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../shared/auth-context';
 import {
   LandingPageContainer
 } from './LandingPage.styled';
 
 const LandingPage = () => {
+  const user = useContext(AuthContext);
+
+  if (!user.isLoggedIn) {
+    return (
+      <LandingPageContainer>
+        <Typography variant='h2'>Welcome to Squirrel Saver!</Typography>
+        <Typography variant='subtitle2'>*The app designed to help squirrel away money in order to help build your savings.*</Typography>
+        <Typography variant='h5' mt={7}>Already have an account? Great, welcome back!</Typography>
+        <Typography variant='h5'><Link to={`${PossibleRoutes.LOGIN}`}>Click here to login.</Link></Typography>
+        <Typography variant='h5' mt={7}>First time here? Awesome, welcome to Squirrel Saver!</Typography>
+        <Typography variant='h5'><Link to={`${PossibleRoutes.CREATE_ACCOUNT}`}>Click here to create an account.</Link></Typography>
+      </LandingPageContainer>
+    )
+  };
+
   return (
-    <LandingPageContainer>
-      <Typography variant='h2'>Welcome to Squirrel Saver!</Typography>
-      <Typography variant='subtitle2'>*The app designed to help squirrel away money in order to help build your savings.*</Typography>
-      <Typography variant='h5' mt={7}>Already have an account? Great, welcome back!</Typography>
-      <Typography variant='h5'><Link to={`${PossibleRoutes.LOGIN}`}>Click here to login.</Link></Typography>
-      <Typography variant='h5' mt={7}>First time here? Awesome, welcome to Squirrel Saver!</Typography>
-      <Typography variant='h5'><Link to={`${PossibleRoutes.CREATE_ACCOUNT}`}>Click here to create an account.</Link></Typography>
-    </LandingPageContainer>
-  )
-}
+    <div>HomePage</div>
+  );
+};
 
 export default LandingPage;
