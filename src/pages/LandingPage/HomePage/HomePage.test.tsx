609993d37851a8de9a822fff0dc428aed9e93483
Lucas Merchant <lbmerchant93@gmail.com>
2023-05-18 11:35:54 -0600

    Write up initial test descriptions for HomePage

--- src/pages/LandingPage/HomePage/HomePage.test.tsx
+++ src/pages/LandingPage/HomePage/HomePage.test.tsx
@@ -17,4 +17,32 @@ describe('Landing Page', () => {
         const welcomeMessage = screen.getByText("Welcome back!");
         expect(welcomeMessage).toBeInTheDocument();
     });
+
+    it.skip('Renders a way for the user to select their target savings range if they don\'t already have one. Will also explain the details of how the app works.', () => {
+        render(<HomePage user={testUser} />);
+    });
+
+    it.skip('Renders the saving range the user has selected.', () => {
+        render(<HomePage user={testUser} />);
+    });
+
+    it.skip('Renders the numbers the user has drawn in ascending order.', () => {
+        render(<HomePage user={testUser} />);
+    });
+
+    it.skip('Renders the numbers the user has drawn in drawing order.', () => {
+        render(<HomePage user={testUser} />);
+    });
+
+    it.skip('Renders an illustration of how close the user is to their total savings goal.', () => {
+        render(<HomePage user={testUser} />);
+    });
+
+    it.skip('Renders how many times the user has drawn numbers.', () => {
+        render(<HomePage user={testUser} />);
+    });
+
+    it.skip('Renders the most previously drawn number.', () => {
+        render(<HomePage user={testUser} />);
+    });
 });
